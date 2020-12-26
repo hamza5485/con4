d6a882d03a6d73946c22be4f6f159eefbdd60879
Hamza Anas <hamza.anas@itinnovate.com.au>
2020-12-26 20:56:01 +0500

    components setup

---
+++ src/components/app/Board/Board.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Board = () => {
+    return (
+        <p>hello from Board :)</p>
+    );
+};
+
+export default Board;
---
+++ src/components/app/Piece/Piece.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Piece = () => {
+    return (
+        <p>hello from Piece :)</p>
+    );
+};
+
+export default Piece;
