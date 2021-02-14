b497a85af45cc5bc17b6a2e28ea199f5b17ec2cc
Hamza Anas <hamza.anas@itinnovate.com.au>
2021-02-14 13:58:35 +0500

    added <React.Suspense> for lazy loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import Board from "./components/app/Board"
+import React from 'react';
+const Board = React.lazy(() => import('./components/app/Board'));
 
 const App = () => {
     return (
-        <div className="App">
-            <Board />
-        </div>
+        <React.Suspense fallback={
+            <div>
+                Loading...
+            </div>
+        }>
+            <div className="App">
+                <Board />
+            </div>
+        </React.Suspense>
     );
 };
 
